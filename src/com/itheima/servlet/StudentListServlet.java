c9f7f3b5cd044ebda33f0dbdcc04d9bb7b270466
Wilson <979446886@qq.com>
2019-08-02 13:17:21 +0800

    Initial commit

---
+++ src/com/itheima/servlet/StudentListServlet.java
@@ -0,0 +1,23 @@
+package com.itheima.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 负责查询所有的学生信息.然后呈现到list.jsp页面上.
+ *
+ */
+@WebServlet(name = "StudentListServlet",urlPatterns = "/studentList")
+public class StudentListServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
